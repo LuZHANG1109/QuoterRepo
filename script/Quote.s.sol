884134e4ea872ada1aa7fcff0a828205cb39a959
lu.zhang <lu.zhang@okg.com>
2025-08-04 13:18:13 +0800

    add Quote

---
+++ script/Quote.s.sol
@@ -0,0 +1,65 @@
+pragma solidity 0.8.17;
+
+import "forge-std/test.sol";
+import "forge-std/console2.sol";
+import "../src/Quote.sol";
+
+contract Deploy is Test {
+    QueryData quoter;
+    address deployer = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
+    // base
+    // address stateView = 0xA3c0c9b65baD0b08107Aa264b0f3dB444b867A71;
+    // address positionManager = 0x7C5f5A4bBd8fD63184577525326123B519429bDc;
+    // eth
+    // address stateView = 0x7fFE42C4a5DEeA5b0feC41C94C136Cf115597227;
+    // address positionManager = 0xbD216513d74C8cf14cf4747E6AaA6420FF64ee9e;
+    // Arbitrum
+    // address stateView = 0x76Fd297e2D437cd7f76d50F01AfE6160f86e9990;
+    // address positionManager = 0xd88F38F930b7952f2DB2432Cb002E7abbF3dD869;
+    // Optimism
+    // address stateView = 0xc18a3169788F4F75A170290584ECA6395C75Ecdb;
+    // address positionManager = 0x3C3Ea4B57a46241e54610e5f022E5c45859A1017;
+    // Polygon
+    // address stateView = 0x5eA1bD7974c8A611cBAB0bDCAFcB1D9CC9b3BA5a;
+    // address positionManager = 0x1Ec2eBf4F37E7363FDfe3551602425af0B3ceef9;
+    // Blast
+    // address stateView = 0x12a88AE16F46DCe4e8B15368008Ab3380885df30;
+    // address positionManager = 0x4AD2F4CcA2682cBB5B950d660dD458a1D3f1bAaD;
+    // Avalanche
+    // address stateView = 0xc3c9e198C735a4b97e3e683f391cCBDD60B69286;
+    // address positionManager = 0xB74b1F14d2754AcfcbBe1a221023a5cf50Ab8ACD;
+    // BNB Smart Chain
+    // address stateView = 0xd13Dd3D6E93f276FAfc9Db9E6BB47C1180aeE0c4;
+    // address positionManager = 0x7A4a5c919aE2541AeD11041A1AEeE68f1287f95b;
+    // UniChain
+    address stateView = 0x86e8631A016F9068C3f085fAF484Ee3F5fDee8f2;
+    address positionManager = 0x4529A01c7A0410167c5740C487A8DE60232617bf;
+
+    function run() public {
+        require(deployer == 0x399EfA78cAcD7784751CD9FBf2523eDf9EFDf6Ad, "wrong deployer! change the private key");
+        // base
+        // vm.createSelectFork("https://base.llamarpc.com");
+        // eth
+        // vm.createSelectFork("https://eth.blockrazor.xyz");
+        // Arbitrum 42161
+        // vm.createSelectFork(vm.envString("ARB_RPC_URL"));
+        // Optimism
+        // vm.createSelectFork(vm.envString("OP_RPC_URL"));
+        // Polygon
+        // vm.createSelectFork(vm.envString("POLYGON_RPC_URL"));
+        // Blast 81457
+        // vm.createSelectFork(vm.envString("BLAST_RPC_URL"));
+        // Avalanche
+        // vm.createSelectFork(vm.envString("AVAX_RPC_URL"));
+        // BNB Smart Chain
+        // vm.createSelectFork("https://binance.llamarpc.com");
+        // unichain
+        vm.createSelectFork("https://mainnet.unichain.org");
+
+        vm.startBroadcast(deployer);
+        require(block.chainid == 130, "must be right chain");
+        quoter = new QueryData(stateView, positionManager);
+        console2.log("query address", address(quoter));
+        vm.stopBroadcast();
+    }
+}
---
+++ test/Quote.t.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+pragma abicoder v2;
+
+import "forge-std/console2.sol";
+import "forge-std/test.sol";
+import {QueryData} from "../src/Quote.sol";
+
+contract UniV4QuoterTest is Test{
+    QueryData quoter = QueryData(0x8F8Bd31d1B9e8E15c0E36dC5b2645cfE4b5713BA);
+    // address stateView = 0x7fFE42C4a5DEeA5b0feC41C94C136Cf115597227;
+    // address positionManager = 0xbD216513d74C8cf14cf4747E6AaA6420FF64ee9e;
+    // base
+    address stateView = 0xA3c0c9b65baD0b08107Aa264b0f3dB444b867A71;
+    address positionManager = 0x7C5f5A4bBd8fD63184577525326123B519429bDc;
+    // unichain
+    // QueryData quoter = QueryData(0xf3964a4Ba7371E4C44aADFAc679cE9ba8B6AdC66);
+
+    function setUp() public {
+        // vm.createSelectFork("https://mainnet.unichain.org");
+        vm.createSelectFork("https://ethereum.rpc.subquery.network/public");
+        // vm.createSelectFork("https://base.rpc.subquery.network/public");
+        // quoter = new QueryData(stateView, positionManager);
+    }
+
+    function test_univ4Quoter() public {
+        // QueryData.PoolKey memory key = QueryData.PoolKey({
+        //     currency0: QueryData.Currency.wrap(address(0)), // ETH
+        //     currency1: QueryData.Currency.wrap(0xdAC17F958D2ee523a2206206994597C13D831ec7), // USDT
+        //     fee: 500, 
+        //     tickSpacing: 10,
+        //     hooks: IHooks(address(0))
+        // });
+
+        // bytes32 poolId = 0x72331FCB696B0151904C03584B66DC8365BC63F8A144D89A773384E3A579CA73;
+        bytes32 poolId = 0x811a9d9ae89f0bd308e8c2d5f66e98974069f8b9e239b7154438b461b447bd99;
+        // unichain
+        // bytes32 poolId = 0x25939956ef14a098d95051d86c75890cfd623a9eeba055e46d8dd9135980b37c;
+        bytes memory tickInfo = quoter.queryUniv4TicksSuperCompact(poolId, 250);
+        uint256 len;
+        uint256 offset;
+        assembly {
+            len := mload(tickInfo)
+            offset := add(tickInfo, 32)
+        }
+        for (uint256 i = 0; i < len / 32; i++) {
+            int256 res;
+            assembly {
+                res := mload(offset)
+                offset := add(offset, 32)
+            }
+            console2.log("tick: %d", int128(res >> 128));
+            console2.log("l: %d", int128(res));
+        }
+    }
+}
